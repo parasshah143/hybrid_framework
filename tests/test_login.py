825876c9eb5c6a489d6857cc49a189d6c1cf46aa
parasshah143 <parasshah25@gmail.com>
2023-03-02 12:29:07 +0530

    Added Pytest Fixture

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,24 +1,27 @@
+import pytest
 from selenium import webdriver
 from assertpy import assert_that
 from selenium.webdriver.common.by import By
 
 
 class TestLoginUI:
+
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(20)
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        yield
+        self.driver.quit()
+
     def test_title(self):
-        driver = webdriver.Chrome()
-        driver.maximize_window()
-        driver.implicitly_wait(20)
-        driver.get("https://opensource-demo.orangehrmlive.com/")
-        actual_title = driver.title
+        actual_title = self.driver.title
         print(actual_title)
         # assert actual_title == "OrangeHRM"
         assert_that("OrangeHRM").is_equal_to(actual_title)
 
     def test_header(self):
-        driver = webdriver.Chrome()
-        driver.maximize_window()
-        driver.implicitly_wait(20)
-        driver.get("https://opensource-demo.orangehrmlive.com/")
-        header = driver.find_element(By.XPATH, "//h5[@class='oxd-text oxd-text--h5 orangehrm-login-title']").text
+        header = self.driver.find_element(By.XPATH, "//h5[@class='oxd-text oxd-text--h5 orangehrm-login-title']").text
         print(header)
         assert_that("Login").is_equal_to(header)
