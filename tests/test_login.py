0b26a533655f3a2ec9ead5e8117f38287c201184
parasshah143 <parasshah25@gmail.com>
2023-03-02 12:02:26 +0530

    Added Pytest Header

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from assertpy import assert_that
+from selenium.webdriver.common.by import By
 
 
 class TestLoginUI:
@@ -12,3 +13,12 @@ class TestLoginUI:
         print(actual_title)
         # assert actual_title == "OrangeHRM"
         assert_that("OrangeHRM").is_equal_to(actual_title)
+
+    def test_header(self):
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.implicitly_wait(20)
+        driver.get("https://opensource-demo.orangehrmlive.com/")
+        header = driver.find_element(By.XPATH, "//h5[@class='oxd-text oxd-text--h5 orangehrm-login-title']").text
+        print(header)
+        assert_that("Login").is_equal_to(header)
