b77bc850fd0d1564f909584ae4655628174fb21c
parasshah143 <parasshah25@gmail.com>
2023-03-06 11:48:06 +0530

    Added Pytest Fixture

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,19 +1,14 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from assertpy import assert_that
 from selenium.webdriver.common.by import By
 
 
-def scope_class():
-    print("class triggered")
-    yield
-    print("class end")
-
-
-class TestLoginUI:
-
+class WebDriverWrapper:
     @pytest.fixture(scope="function", autouse=True)
-    def setup(self):
+    def browser_config(self):
+
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
         self.driver.implicitly_wait(20)
@@ -21,28 +16,21 @@ class TestLoginUI:
         yield
         self.driver.quit()
 
-    def test_title(self):
-        actual_title = self.driver.title
-        print(actual_title)
-        # assert actual_title == "OrangeHRM"
-        assert_that("OrangeHRM").is_equal_to(actual_title)
 
-    def test_header(self):
-        header = self.driver.find_element(By.XPATH, "//h5[@class='oxd-text oxd-text--h5 orangehrm-login-title']").text
-        print(header)
-        assert_that("Login").is_equal_to(header)
-
+class TestLogin(WebDriverWrapper):
     def test_valid_login(self):
         self.driver.find_element(By.NAME, "username").send_keys("Admin")
         self.driver.find_element(By.NAME, "password").send_keys("admin123")
         self.driver.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
-        text12 = self.driver.find_element(By.XPATH, "//h6[normalize-space()='Dashboard']").text
-        assert_that("Dashboard").is_equal_to(text12)
+        actual_text = self.driver.find_element(By.XPATH, "//h6[normalize-space()='Dashboard']").text
+        assert_that("Dashboard").is_equal_to(actual_text)
 
 
-# class TestLogin(TestLoginUI):
-#     def test_valid_login(self):
-#         print("valid login")
-#
-#     def test_invalid_login(self):
-#         print("Invalid login")
+class TestLoginUI(WebDriverWrapper):
+    def test_title(self):
+        actual_title = self.driver.title
+        assert_that("OrangeHRM").is_equal_to(actual_title)
+
+    def test_header(self):
+        actual_header = self.driver.find_element(By.XPATH, "//h5").text
+        assert_that("Login").is_equal_to(actual_header)
